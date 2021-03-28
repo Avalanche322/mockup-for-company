569231d6d69d47c3fd3029bd108c7e759074b79d
Avalanche322 <avalfnche@gmail.com>
2021-03-28 17:31:50 +0300

    added bloks: hero, easy & useful, services and reviews

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
 	$('.spoiler__title').click(function(event){
 		$(this).toggleClass('block').next().slideToggle("slow")
-	})
+	});
+	$('.reviews__slider').slick({
+		vertical: true,
+		verticalSwiping: true,
+		slidesToShow: 2,
+		autoplay: true,
+		customPaging: '1200px',
+		accessibility: false,
+		prevArrow: '<img src="/img/reviews/arrow_up_white.png">',
+		nextArrow: '<img src="/img/reviews/arrow_down_white.png">'
+  });
 })
 $(document).ready(function () {
 	$('#burger').click(function (event) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,58 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+	  colors: {
+		  transparent: 'transparent',
+		  current: 'currentColor',
+		  black: colors.black,
+		  white: colors.white,
+		  blue: {
+			  light: '#80d8ff',
+			  DEFAULT: '#82b1ff',
+			  dark: '#061e37'
+		  },
+		  purple: {
+			  DEFAULT: '#b388ff'
+		  },
+		  pink: {
+			  DEFAULT: '#ea80fc'
+		  },
+		  gray: {
+			  light: '#b0bec5',
+			  DEFAULT: '#78909c',
+		  },
+		  lime: {
+			  DEFAULT: '#1de9b6'
+		  }
+	  },
     extend: {
-		 container: {
+		container: {
 			 center: true,
-		 },
-		fontFamily: {
-			'body': ['Raleway', 'sans-serif']
 		},
 		lineHeight: {
 			'14': '4.063rem',
 		},
-		boxShadow: {
-			'3xl': '0px 5px 5px 0px rgba(0,0,0,0.15)',
+		fontFamily: {
+			'body': ['Raleway', 'sans-serif'],
 		},
-		  colors: {
-			  blue: {
-				  light: '#80d8ff',
-				  DEFAULT: '#82b1ff'
-			  },
-			  purple:{
-				  light: '#b388ff'
-			  },
-			  pink:{
-				  light: '#ea80fc'
-			  },
-			  black:{
-				  light: '#061e37'
-			  }
-		  }
+		boxShadow: {
+			  '3xl': '0px 5px 5px 0px rgba(0,0,0,0.15)',
+		},
+		screens: {
+			'extra-md': {'min': '920px' },
+		},
+		fontSize: {
+			'none': '0rem',
+		},
 	 },
   },
   variants: {
     extend: {
 		  translate: ['active', 'group-hover'],
 		  opacity: ['active'],
+		  boxShadow: ['active'],
 	 },
   },
   plugins: [],
