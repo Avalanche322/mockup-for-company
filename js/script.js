b0636b98a600525860759739151c7cf3871156d1
Avalanche322 <avalfnche@gmail.com>
2021-04-02 09:11:45 +0300

    added sticky bar

--- js/script.js
+++ js/script.js
@@ -50,3 +50,7 @@ $(document).ready(function(){
 		]
 	});
 })
+window.addEventListener('scroll', function(){
+	let header = document.querySelector('header');
+	header.classList.toggle('sticky', window.scrollY > 0);
+});
